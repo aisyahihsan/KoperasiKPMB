8025de2280e36b50da18597e229935558ded8cd8
Nur Aisyah Mohamad Ihsan <64122100+aisyahihsan@users.noreply.github.com>
2023-10-26 10:35:10 +0800

    Add files via upload

---
+++ KoperasiKPMB/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace KoperasiKPMB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbMenu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtActual = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbMenu
+            // 
+            this.cmbMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMenu.FormattingEnabled = true;
+            this.cmbMenu.Items.AddRange(new object[] {
+            "Book",
+            "Pen",
+            "Pencil",
+            "Ruler"});
+            this.cmbMenu.Location = new System.Drawing.Point(214, 25);
+            this.cmbMenu.Name = "cmbMenu";
+            this.cmbMenu.Size = new System.Drawing.Size(198, 37);
+            this.cmbMenu.TabIndex = 0;
+            this.cmbMenu.SelectedIndexChanged += new System.EventHandler(this.cmbMenu_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MENU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "QUANTITY";
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQuantity.Location = new System.Drawing.Point(214, 96);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(198, 34);
+            this.txtQuantity.TabIndex = 3;
+            // 
+            // txtActual
+            // 
+            this.txtActual.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtActual.Location = new System.Drawing.Point(315, 211);
+            this.txtActual.Name = "txtActual";
+            this.txtActual.Size = new System.Drawing.Size(198, 34);
+            this.txtActual.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ACTUAL PRICE";
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiscount.Location = new System.Drawing.Point(315, 283);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(198, 34);
+            this.txtDiscount.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(250, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DISCOUNTED PRICE";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.HotPink;
+            this.btnCalculate.Font = new System.Drawing.Font("Waltograph UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(145, 159);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(218, 35);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "CALCULATE";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TOTAL PRICE";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(315, 398);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(198, 34);
+            this.txtTotal.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 530);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtActual);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbMenu);
+            this.Name = "Form1";
+            this.Text = "Koperasi KPMB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbMenu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtActual;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
+
---
+++ KoperasiKPMB/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoperasiKPMB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void cmbMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double price=0;
+
+            if (cmbMenu.Text == "Book")
+            {
+                price = 3.50;
+            }
+
+            else if (cmbMenu.Text == "Pen")
+            {
+                price = 2.50;
+            }
+
+            else if (cmbMenu.Text == "Pencil")
+            {
+                price = 1.50;
+            }
+
+            else if (cmbMenu.Text == "Ruler")
+            {
+                price = 2.50;
+            }
+
+            int quantity = int.Parse(txtQuantity.Text);
+
+            double actual=0;
+            actual = price * quantity;
+            txtActual.Text = actual.ToString();
+
+            double discount=0;
+
+            if (quantity <11)
+            {
+               
+            }
+
+            else if (quantity>=11 && quantity<=20)
+            {
+               discount= actual * 0.05;
+            }
+
+            else if (quantity>=21 && quantity<=50)
+            {
+                discount = actual * 0.1;
+            }
+
+            else if (quantity>50)
+            {
+                discount = actual * 0.15;
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid quantity");
+            }
+
+            txtDiscount.Text = discount.ToString();
+
+            double total = 0;
+            total = actual-discount;
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnCalculateTotal_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
